6935bd31b70f977e4d15503a24d36ab43a7c29b0
katreddyvenkateswaraReddy <63310358+katreddyvenkateswaraReddy@users.noreply.github.com>
2024-01-27 12:35:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function insert_Row() {
     //Write your code here
+	var table = document.getElementById("sampleTable");
+    var newRow = table.insertRow(0); // Inserts a new row at index 1 (just after the header row)
+
+    var cell1 = newRow.insertCell(0); // Inserts a cell at index 0
+    var cell2 = newRow.insertCell(1);
+	// Inserts a cell at index 1
+
+    // You can set the content of the cells here, for example:
+    cell1.innerHTML = "New Cell1";
+    cell2.innerHTML = "New Cell2";
+	 
+	
   
   
 }
